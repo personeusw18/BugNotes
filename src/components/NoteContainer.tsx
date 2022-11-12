8f35c71655aa50b12c1fe83686cf131f4aeb3a01
personeusw18 <44588428+personeusw18@users.noreply.github.com>
2022-11-12 12:40:07 -0500

    Editable Text Inputs (useState)

--- src/components/NoteContainer.tsx
+++ src/components/NoteContainer.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 export interface Note{
-  name: string;
   message: string;
+  name: string;
   // date?: Date;
 }
 
@@ -13,19 +13,24 @@ interface PropTypes {
 
 export function NoteContainer (props: PropTypes) {
 
-  const [note, setNote] = useState(props.note);
+  const [noteMessage, setNoteMessage] = useState(props.note.message);
+  const [noteName, setNoteName] = useState(props.note.name);
 
-  const handleMessageChange = () => {
 
+  const handleMessageChange = (e: ChangeEvent<HTMLTextAreaElement>)=> {
+    setNoteMessage(e.target.value)
   }
 
+  const handleMessageNote = (e: ChangeEvent<HTMLInputElement>) => {
+    setNoteName(e.target.value)
+  }
 
   return (
     <div className="bg-yellow-50 text-xl min-h-[100px] font-cursive m-6 mb-0 p-10 pt-4 pb-4 w-min
         flex flex-col shadow-md justify-between" 
     >
-      <textarea onChange={handleMessageChange} className="bg-yellow-50 text-zinc-900 placeholder-zinc-900 h-96 resize max-w-full min-w-[200px]" value={note.message}/>
-      <input className="bg-yellow-50 text-zinc-900 placeholder-zinc-900 " type="text" value={"-" + note.name}/>
+      <textarea onChange={handleMessageChange} className="bg-yellow-50 text-zinc-900 placeholder-zinc-900 h-96 resize max-w-full min-w-[200px]" value={noteMessage}/>
+      <input onChange={handleMessageNote} className="bg-yellow-50 text-zinc-900 placeholder-zinc-900 " type="text" value={noteName}/>
     </div>
   )
 }
