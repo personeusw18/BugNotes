18798301b1c8fa253155bc8b21fd97fa897b2441
personeusw18 <44588428+personeusw18@users.noreply.github.com>
2022-11-12 17:30:54 -0500

    Fixed Initial Height of Note

--- src/components/NoteContainer.tsx
+++ src/components/NoteContainer.tsx
@@ -31,10 +31,10 @@ export function NoteContainer (props: NotePropTypes) {
 
   return (
     <Draggable bounds={"parent"}>
-      <div className="bg-yellow-50 text-xl w-[300px] min-h-[150px] h-fit font-cursive p-4
+      <div className="bg-yellow-50 text-xl w-[300px] h-fit font-cursive p-4
           flex flex-col shadow-lg justify-between border-zinc-400 border-opacity-25 m-3 border-2 absolute" 
       >
-        <div id="pin">
+        <div id="pin" className="w-full h-full bg-black">
           <div className="absolute left-[calc(50%-13px)] -top-8 w-6 h-6 rounded-full bg-gradient-to-tr from-rose-900 to-rose-600 outline-1 outline-zinc-800"/>
           <div className="absolute left-[calc(50%-4px)] top-[12px] w-1.5 h-1.5 rounded-full opacity-25 bg-zinc-700 outline-zinc-800"/>
           <div className="absolute left-[calc(50%-2px)] w-0.5 h-6 -top-2 bg-gradient-to-r from-zinc-600 to-zinc-500 rounded-b-full"/>
@@ -44,9 +44,10 @@ export function NoteContainer (props: NotePropTypes) {
         <textarea 
           onChange={handleMessageChange} 
           spellCheck={false}
+          maxLength={500}
           style={{height: noteMessageHeight}}
-          className="bg-yellow-50 text-zinc-900 placeholder-zinc-900 focus:outline-none 
-            focus:border-zinc-400 focus:outline-dashed focus:outline-2 focus:outline-zinc-300 min-h-[28px] resize-none" 
+          className="bg-yellow-50 box-border text-zinc-900 placeholder-zinc-900 focus:outline-none overflow-auto
+            focus:border-zinc-400 focus:outline-dashed focus:outline-2 focus:outline-zinc-300 min-h-[2rem] resize-none" 
           value={noteMessage}/>
 
         <div id="name-div" className="flex w-fit pt-2">
