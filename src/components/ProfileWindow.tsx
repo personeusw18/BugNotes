c911cd251c0fe3b0088f6cb491c4951b7f062866
personeusw18 <44588428+personeusw18@users.noreply.github.com>
2022-11-17 18:31:14 -0500

    init profilewindow component

--- src/components/ProfileWindow.tsx
+++ src/components/ProfileWindow.tsx
@@ -1,7 +1,12 @@
 export function ProfileWindow () {
     return (
-      <div className="m-0 p-0 bg-blue-50 justify-center w-screen max-w-full max-h-fit">
-      
+      <div className="m-0 justify-center w-full h-full text-center font-cursive p-10 flex flex-col gap-10">
+        <h1 className="text-3xl">Your Boards</h1>
+        <h1 className="text-3xl">Settings</h1>
+        <form className="w-1/2 h-4/5 m-auto bg-yellow-50 text-xl">
+          <p>Name</p>
+
+        </form>
       </div>
     )
   }
